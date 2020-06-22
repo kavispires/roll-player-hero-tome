d98ca2e61c95a14763a27a771b4e1a7fd7c92b87
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2020-06-21 19:26:10 -0700

    Updates

---
+++ precache-manifest.339a3ada802f8aabb84142f3989a6873.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0efd21eed3002ec3eab46e11775381f",
+    "url": "/roll-player-hero-tome/index.html"
+  },
+  {
+    "revision": "2eadfc3571bfb864f71b",
+    "url": "/roll-player-hero-tome/static/css/main.070bc2d0.chunk.css"
+  },
+  {
+    "revision": "efd603eccee2c3bee2e4",
+    "url": "/roll-player-hero-tome/static/js/2.1f7c5643.chunk.js"
+  },
+  {
+    "revision": "16e72e8a7b17b4817847b2ca58cd85b3",
+    "url": "/roll-player-hero-tome/static/js/2.1f7c5643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eadfc3571bfb864f71b",
+    "url": "/roll-player-hero-tome/static/js/main.c80ca5a8.chunk.js"
+  },
+  {
+    "revision": "056dba8b2eb53721a926",
+    "url": "/roll-player-hero-tome/static/js/runtime-main.76a7e627.js"
+  }
+]);
