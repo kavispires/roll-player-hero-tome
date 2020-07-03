150299a16a32dbd09109583fdb3defe77e806ed0
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2020-07-03 15:30:15 -0700

    Updates

--- precache-manifest.07b638094e5fd20fc49824ff148edf5f.js
+++ precache-manifest.07b638094e5fd20fc49824ff148edf5f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5a0e9ad4c1b63554713a87eabe303718",
+    "revision": "71d1164ba7dd8352cabd68e0979642af",
     "url": "/roll-player-hero-tome/index.html"
   },
   {
-    "revision": "ae4ce5f5f0306fc87510",
+    "revision": "0cc7a8dbe15ce7f37f25",
     "url": "/roll-player-hero-tome/static/css/main.257777c4.chunk.css"
   },
   {
-    "revision": "25cfe66517b01dae4633",
-    "url": "/roll-player-hero-tome/static/js/2.8656cf37.chunk.js"
+    "revision": "5e12fd9674cd0e09dda7",
+    "url": "/roll-player-hero-tome/static/js/2.b37d31f5.chunk.js"
   },
   {
     "revision": "16e72e8a7b17b4817847b2ca58cd85b3",
-    "url": "/roll-player-hero-tome/static/js/2.8656cf37.chunk.js.LICENSE.txt"
+    "url": "/roll-player-hero-tome/static/js/2.b37d31f5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ae4ce5f5f0306fc87510",
-    "url": "/roll-player-hero-tome/static/js/main.bfb53b88.chunk.js"
+    "revision": "0cc7a8dbe15ce7f37f25",
+    "url": "/roll-player-hero-tome/static/js/main.756e8f1e.chunk.js"
   },
   {
     "revision": "056dba8b2eb53721a926",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/roll-player-hero-tome/precache-manifest.228d0b3a808e0dbd9e1ba206df6e196b.js"
+  "/roll-player-hero-tome/precache-manifest.07b638094e5fd20fc49824ff148edf5f.js"
 );
 
 self.addEventListener('message', (event) => {
